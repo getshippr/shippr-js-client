70308a0e718f05f2edd09be7d601bfbf298364e4
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-28 20:22:24 +0200

    add emit and broadcast

--- client/types.ts
+++ client/types.ts
@@ -2,11 +2,30 @@ import SuperSocket from "@shippr/supersocket";
 
 export type DataCallBack = (data: any, err: any) => void;
 export type ShipprClient = {
+  /**
+   * Subscribe to a specific channel
+   * ex
+   * const watcher = await subscribe('my-channel')
+   * watcher.on((data, err) => {
+   *  doSomething()
+   * })
+   **/
   subscribe: (channelId: string) => Promise<ShipprSub>;
+  /**
+   * Send a POST query with data and will publish message to ALL subscribers
+   **/
   publish: (channelId: string, data: any) => Promise<Response>;
 };
 export type ShipprSub = {
   on: (cb: DataCallBack) => void;
   getSocket: () => SuperSocket;
   disconnect: () => void;
+  /**
+   * emit data to only the current socket
+   **/
+  emit: (data: any) => void;
+  /**
+   * broadcast data to all subscribers
+   **/
+  broadcast: (data: any) => void;
 };
